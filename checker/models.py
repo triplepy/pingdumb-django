361756f9e663a10eec8b9e73162d025b18958ab9
jelly <chm073@gmail.com>
2016-11-26 01:45:21 +0900

    Add site model

--- checker/models.py
+++ checker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from model_utils.models import TimeStampedModel
+
+from pingdumb.users.models import User
+
+
+class Site(TimeStampedModel):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    url = models.CharField(max_length=65536)
+
