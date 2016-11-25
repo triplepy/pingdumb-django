308cf26fffa68f2283da233951e9108decaf9678
jelly <chm073@gmail.com>
2016-11-26 01:58:33 +0900

    Add HttpStatus model and add status field on Site model

--- checker/models.py
+++ checker/models.py
@@ -9,3 +9,11 @@ class Site(TimeStampedModel):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=80)
     url = models.CharField(max_length=65536)
+    status = models.ManyToManyField(HttpStatus)
+
+
+class HttpStatus(models.Model):
+    code = models.IntegerField
+    message = models.CharField(max_length=2048)
+
+
