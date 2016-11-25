a36f129388f07c290dd404e0c96c26efca8ddc6a
jelly <chm073@gmail.com>
2016-11-26 01:46:48 +0900

    Remove unused comment

--- checker/models.py
+++ checker/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 from model_utils.models import TimeStampedModel
 
 from pingdumb.users.models import User
@@ -10,4 +9,3 @@ class Site(TimeStampedModel):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=80)
     url = models.CharField(max_length=65536)
-
